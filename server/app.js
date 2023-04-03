d61b22a1eeb4a7986587326d74f40553eb13b28c
Miguel <miguelromeroarjona@gmail.com>
2023-04-03 14:42:49 +0200

    feat(#10): Add login endpoint

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 const usersRouter = require('./controllers/users')
 const zumbiesRouter = require('./controllers/zumbies')
 
@@ -16,8 +17,9 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(express.json())
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
-app.use("/api/zumbies", zumbiesRouter)
+app.use('/api/zumbies', zumbiesRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
