2a957f93d86219665257c856fe72c23ba1d58a89
migromarj <miguelromeroarjona@gmail.com>
2023-04-12 13:38:22 +0200

    feat(init): Add color palette to tailwind configuration

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-dark-blue">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-dark-purple">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link "
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <div className="mt-10 ml-10 text-light-gray">
+          Click the following button
+        </div>
+        <button className="mb-10 mt-10 ml-10 p-2 rounded-lg bg-gold text-dark-purple">
+          Click me!
+        </button>
       </header>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue": "#112D4E", // Background color
+        "light-gray": "#D6DADB", // Letter color
+        "gold": "#E7B10A", // Button color
+        "dark-purple": "#3B1F6E", // Button Letter color
+      },
+    },
   },
   plugins: [],
-}
+};
