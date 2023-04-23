509bfad65a0d787908835d53f5e95c57d41a29d0
migromarj <miguelromeroarjona@gmail.com>
2023-04-23 17:00:07 +0200

    feat(#014): Add footer component

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,21 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RegisterForm from './components/RegisterForm'; 
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className='App bg-dark-blue w-full h-full'>
-      <BrowserRouter>
-        <Routes>
-          
+    <div className="App w-full h-full">
+      <div className="bg-dark-blue w-full h-full">
+        <BrowserRouter>
+          <Routes>
             <Route path="/" element={<h1> Home </h1>} />
             <Route path="/register" element={<RegisterForm />} />
             <Route path="*" element={<h1> 404 </h1>} />
-        
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <Footer />
     </div>
   );
 }
